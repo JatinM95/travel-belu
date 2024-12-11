618b3d299dab0ce8e423a73abe954079323f6769
Jatin <mangaljatin.100@gmail.com>
2024-12-12 03:03:31 +0530

    correction done

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import Navbar1 from "@/components/Navbar/Navbar.jsx";
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar1 from "@/components/Navbar/Navbar.jsx";
 import Slider from "@/components/Slider/Slider.jsx";
 import Footer from "@/components/Footer/Footer.jsx";
 import logo from '@/assets/images/logo/belu_logo.png';
