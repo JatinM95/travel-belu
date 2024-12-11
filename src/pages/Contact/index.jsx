c4d11c5312171af599b524f301275d8ea2938227
Jatin <mangaljatin.100@gmail.com>
2024-12-12 02:09:37 +0530

    changes done

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar1 from '../../components/Navbar/Navbar'
-import ContactUs from '../../components/ContactUs/ContactUs'
-import Footer from '../../components/Footer/Footer'
+import Navbar1 from '../../components/Navbar/Navbar.jsx'
+import ContactUs from '../../components/ContactUs/ContactUs.jsx'
+import Footer from '../../components/Footer/Footer.jsx'
 import logo from '../../assets/images/logo/belu_logo.png'
 // assets/images/Frame_1116605564.png
 // import AppStore from '../../components/AppStore/AppStore'
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import Navbar1 from "../../components/Navbar/Navbar";
+import Navbar1 from "../../components/Navbar/Navbar.jsx";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Slider from '../../components/Slider/Slider';
-import Footer from "../../components/Footer/Footer";
+import Slider from '../../components/Slider/Slider.jsx';
+import Footer from "../../components/Footer/Footer.jsx";
 import logo from '../../assets/images/logo/belu_logo.png'
 
 function Home() {
